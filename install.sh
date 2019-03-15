a7b297fed0cf72dbd023afe3ce96781351b2e0a6
Luca Ottaviano <lottaviano@develer.com>
2019-03-15 14:37:15 +0100

    Add kp command to kill running UI process

--- install.sh
+++ install.sh
@@ -1,2 +1,11 @@
+#!/usr/bin/env bash
+set -eou pipefail
+cd "$(dirname "$0")/"
+if [ "$#" -lt 1 ]; then
+    echo "Usage: $0 [exe name]"
+    exit 1
+fi
+
 ssh root@192.168.129.1 'mount -o remount,rw /; passwd -d root'
 scp -r etc home root@192.168.129.1:/
+echo "alias kp='killall $1'" | ssh root@192.168.129.1 "cat >> /home/root/.profile"
